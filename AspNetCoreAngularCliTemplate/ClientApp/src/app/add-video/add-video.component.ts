23290e75e0142579cef3c573c809f514e712239e
ozkan <ozkanbe@gmail.com>
2018-10-28 20:48:07 +0100

    add video component

--- AspNetCoreAngularCliTemplate/ClientApp/src/app/add-video/add-video.component.ts
+++ AspNetCoreAngularCliTemplate/ClientApp/src/app/add-video/add-video.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+ 
+import { VideosService } from '../videos.service';
+import { Video } from "../video";
+ 
 @Component({
   selector: 'app-add-video',
   templateUrl: './add-video.component.html',
   styleUrls: ['./add-video.component.css']
 })
-export class AddVideoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class AddVideoComponent {
+  public genres: string[];
+  public video: Video = {};
+ 
+  constructor(
+    private router: Router,
+    private videosSvc: VideosService) {
+    this.genres = [
+      "",
+      "Music",
+      "Comedy",
+      "Inspirational",
+      "Tech",
+      "News",
+      "Sports"
+    ];
   }
-
+ 
+  addVideo() {
+    this.videosSvc.addVideo(this.video)
+      // The subscribe method here adds a listener to the asynchronous add
+      // operation and the listener is called once the video is added.
+      // Here, we are switching to the video list page after adding the video
+      .subscribe(() => {
+        this.router.navigateByUrl('/videolist');
+      });
+  }
 }
