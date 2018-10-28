468fea20aad3abeb83ca97ff299467b6af26ba8a
ozkan <ozkanbe@gmail.com>
2018-10-28 20:28:14 +0100

    Do API call (db), give it to videos var in component and show in UI

--- AspNetCoreAngularCliTemplate/ClientApp/src/app/video-list/video-list.component.ts
+++ AspNetCoreAngularCliTemplate/ClientApp/src/app/video-list/video-list.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Inject } from '@angular/core';
+ 
+import { VideosService } from '../videos.service';
+import { Video } from "../video";
+import { windowProvider } from '../../window';
+ 
 @Component({
   selector: 'app-video-list',
   templateUrl: './video-list.component.html',
   styleUrls: ['./video-list.component.css']
 })
 export class VideoListComponent implements OnInit {
-
-  constructor() { }
-
+  public videos: Video[];
+ 
+  constructor(private videosService: VideosService,
+    @Inject(windowProvider.provide) private window: Window) { }
+ 
   ngOnInit() {
+    this.videosService.getVideos()
+      .subscribe(videos => {
+        this.videos = videos;
+     });
   }
-
+ 
+  watch(video: Video) {
+    this.videosService.setWatched(video)
+      .subscribe(v => console.log(v));
+    this.window.open(video.link, '_blank');
+  } 
 }
